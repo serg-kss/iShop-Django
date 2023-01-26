28ab63c06cc99236f2df7b0f7ad2caeec9cde0a1
Serg <korobko_ss@ukr.net>
2023-01-26 12:56:43 +0200

    tailwind/flowbite/templates

--- main/views.py
+++ main/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def home (request):
-   return render(request, 'main/main.html')
+   number = 50
+   products = [
+      {
+         'id':number
+      }
+   ]
+     
+   data = {
+      'title': 'Головна сторінка',
+      'products': products
+   }  
+   return render(request, 'main/main.html', data)
