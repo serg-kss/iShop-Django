484ce5e60a0235213d6be9ed6b838a94c203660f
Serg <korobko_ss@ukr.net>
2023-01-26 16:56:14 +0200

    DB

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Products, Orders, Supplier
+
+admin.site.register(Products)
+admin.site.register(Orders)
+admin.site.register(Supplier)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Supplier(models.Model):
+   
+   name = models.CharField('Поставщик', max_length=100)
+   address = models.CharField('Адрес', max_length=100)
+   tel = models.CharField('Телефон', max_length=10)
+   
+   def __str__(self):
+       return self.name
+     
+   class Meta:
+      verbose_name = 'Поставщик'
+      verbose_name_plural = 'Поставщики'
+ 
+
+class Products(models.Model):
+   
+   title = models.CharField('Название Продукта', max_length=100, unique=True)
+   description = models.TextField('Описание Продукта')
+   price = models.DecimalField('Цена', max_digits=7, decimal_places=2)
+   amount = models.IntegerField('Склад', default=0)
+   supplier = models.ForeignKey(Supplier, verbose_name = 'Поставщик', on_delete=models.CASCADE, default='')
+   options_list = (
+       ('S', 'Small'),
+       ('M', 'Medium'),
+       ('L', 'Large'),
+   )
+   options = models.CharField('Опции', max_length=1, choices=options_list, default='')
+     
+   
+   
+   def __str__(self):
+       return self.title
+     
+   class Meta:
+      verbose_name = 'Товар'
+      verbose_name_plural = 'Товары'
+      
+    
+class Orders(models.Model):
+   
+   order_number = models.IntegerField('Номер Заказа', default=0)
+   date = models.DateTimeField('Дата', default=timezone.now)
+   name = models.CharField('Имя', max_length=15)
+   surname = models.CharField('Фамилия', max_length=20)
+   phone = models.CharField('Телефон', max_length=10)
+   
+   def __str__(self):
+       return f'Заказ №{self.name}'
+   
+   class Meta:
+      verbose_name = 'Заказ'
+      verbose_name_plural = 'Заказы'
+   
