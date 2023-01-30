8e144efb58a99d3b496758b7bbc3d319f1e3b737
Serg <korobko_ss@ukr.net>
2023-01-30 16:20:46 +0200

    refactoring main-page/product-page/cart

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
+    path('', views.MainPage.as_view(), name='home-page'),
+    path('product/<int:pk>', views.ProductPage.as_view(), name='product-page'),
     path('contacts', views.contacts, name='contacts-page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from .models import Products
 from cart.forms import CartAddProductForm
+from django.views.generic import ListView, DetailView
 
 
-def home (request):
-   cart_product_form = CartAddProductForm()   
-   data = {
-      'title': 'Головна сторінка',
-      'products': Products.objects.all(),
-      'cart_product_form': cart_product_form
-   }  
-   return render(request, 'main/main.html', data)
+class MainPage(ListView):
+   model = Products
+   template_name = 'main/main.html'
+   context_object_name = 'products'
+   
+   def get_context_data(self, **kwargs):
+      ctx = super(MainPage, self).get_context_data(**kwargs)
+      ctx['title'] = 'Головна сторінка'
+      
+      
+      return ctx
+   
+class ProductPage(DetailView):
+   model = Products
+   template_name = 'main/product.html'
+   context_object_name = 'product'
+   
+   def get_context_data(self, **kwargs):
+      cart_product_form = CartAddProductForm()
+      ctx = super(ProductPage, self).get_context_data(**kwargs)
+      ctx['title'] = 'Головна сторінка'
+      ctx['cart_product_form'] = cart_product_form
+      
+      return ctx
 
 
 def contacts (request):
